b2c518fd10d2e8ba9d51a3282bf2e84fcc97d2fb
pampattobrasil <pampattobrasil@gmail.com>
2026-07-29 12:29:41 -0300

    Delete assets/js/supabase-config.js

--- assets/js/supabase-config.js
+++
@@ -1,7 +0,0 @@
-// Preencha com os dados do seu projeto Supabase.
-window.PAMPATTO_SUPABASE_URL = '';
-window.PAMPATTO_SUPABASE_ANON_KEY = '';
-window.pampattoSupabase = null;
-if (window.PAMPATTO_SUPABASE_URL && window.PAMPATTO_SUPABASE_ANON_KEY && window.supabase) {
-  window.pampattoSupabase = window.supabase.createClient(window.PAMPATTO_SUPABASE_URL, window.PAMPATTO_SUPABASE_ANON_KEY);
-}
